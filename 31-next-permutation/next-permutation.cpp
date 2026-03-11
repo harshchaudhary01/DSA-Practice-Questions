b68df8120c879d91ec82e7e568517d6e04874cbd
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-03-11 22:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (92.43%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2;
-        while(i >= 0 && nums[i] >= nums[i+1]) {
-            i--;
-        }
-        if(i >= 0) {
-            int j = n - 1;
-            while(nums[j] <= nums[i]) {
-                j--;
+        // first find the gola
+        int gola_idx = -1;
+        for(int i = n-1; i > 0; i--){
+            if(nums[i]> nums[i-1]){
+                gola_idx = i-1;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        if(gola_idx != -1){
+            int swap_idx = gola_idx;
+            for(int j = n-1; j >= gola_idx+1; j--){
+                if(nums[j] > nums[gola_idx]){
+                    swap_idx = j;
+                    break;
+                }
+            }
+            swap(nums[gola_idx], nums[swap_idx]);
+        }
+        reverse(nums.begin()+ gola_idx + 1, nums.end());
     }
 };
