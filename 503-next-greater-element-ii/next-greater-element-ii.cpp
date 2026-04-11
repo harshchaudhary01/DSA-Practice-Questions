c273934cfe51dfdd51f7cbc675bbed2df38e3324
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-04-11 07:24:54 +0530

    Time: 4 ms (58.54%) | Memory: 28.5 MB (60.41%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int>res(n,-1);
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i+1; j <= 2*nums.size(); j++){
-                if(nums[j%n] > nums[i]){
-                    res[i] = nums[j%n];
-                    break;
-                }
+        vector<int> res(n, -1);
+        stack<int> st;
+        for (int i = 2*n - 1; i >= 0; i--) {
+            int num = nums[i % n];
+            while (!st.empty() && st.top() <= num) {
+                st.pop();
             }
+            if (i < n) {
+                res[i] = st.empty() ? -1 : st.top();
+            }
+            st.push(num);
         }
         return res;
     }
