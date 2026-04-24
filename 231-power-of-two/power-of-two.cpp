6e0c42288bc62810ff91a28672b836f4f7cf2d20
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-04-24 15:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (11.90%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n-1)) == 0;
+        for(int i = 0; i < 31; i++){
+            int ans = pow(2,i);
+            if(ans == n) return true;
+        }
+        return false;
     }
 };
