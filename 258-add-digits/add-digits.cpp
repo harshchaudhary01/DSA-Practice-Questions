35578da0727ac73994a14f273a8a4ae604865572
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-04-24 16:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (78.16%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int summation(int num){
+        if(num / 10 == 0) return num;
+
+        int res = 0;
+        while(num != 0){
+            res += num%10;
+            num /= 10;
+        }
+        return summation(res);
+    }
+
+    int addDigits(int num) {
+        if(num == 0) return 0;
+        return summation(num);
+    }
+};
