ab76660e6d224240cdacd41fdbee74198a874d80
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-04-29 17:34:58 +0530

    Time: 53 ms (37.10%) | Memory: 90.8 MB (44.56%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> st;
-        for(int k: nums){
-            if(st.count(k)) return true;
-            st.insert(k);
+        unordered_set<int>st;
+        for(int i = 0;i < nums.size(); i++){
+            if(st.count(nums[i])) return true;
+            st.insert(nums[i]);
         }
         return false;
     }
