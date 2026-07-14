74ec8dfc933899ac5668c2a2bcaca9b9c2a7697a
Harsh Kumar Chaudhary <149001418+harshchaudhary01@users.noreply.github.com>
2026-07-14 20:09:06 +0530

    Time: 3 ms (66.68%) | Memory: 14.9 MB (37.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mpp;
         for(int i = 0; i < nums.size(); i++){
-            int left = target - nums[i];
-            if(mpp.find(left) != mpp.end()){
-                return {mpp[left], i};
+            int x = target - nums[i];
+            if(mpp.find(x) != mpp.end()){
+                return {mpp[x], i};
             }
-            mpp.insert({nums[i], i});
+            mpp[nums[i]] =  i;
         }
-        return {0,0};
+        return {};
     }
 };
